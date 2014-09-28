08b408b9b531ac9ddd6b68d58e8f3e18ba3b0a9f
Jacob Seidelin <jseidelin@nihilogic.dk>
2014-09-28 15:19:22 +0200

    fix new paths

--- bat/extract_all_test.bat
+++ bat/extract_all_test.bat
@@ -2,7 +2,7 @@
 set GAMEID=test
 set ASSETPATH="E:\Games\Planetside 2 Test\Resources\Assets"
 
-for /f "delims=" %%a in ('external\date.exe +%%Y%%m%%d_%%H%%M%%S') do @set timestamp=%%a
-mkdir assets\%GAMEID%\all_%timestamp%
-node packer.js extractall %ASSETPATH% assets\%GAMEID%\all_%timestamp%
+for /f "delims=" %%a in ('..\external\date.exe +%%Y%%m%%d_%%H%%M%%S') do @set timestamp=%%a
+mkdir ..\assets\%GAMEID%\all_%timestamp%
+node ..\packer.js extractall %ASSETPATH% ..\assets\%GAMEID%\all_%timestamp%
 pause
